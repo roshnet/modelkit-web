cb610885265a6fa871fad5e63cf9f271ed2c64be
Roshan Sharma <reprogram46@gmail.com>
2021-04-17 21:32:25 +0530

    feat: Get model predictions in the browser

--- pages/model/[uid].js
+++ pages/model/[uid].js
@@ -1,9 +1,10 @@
-import { Card, Col, Row, Typography } from 'antd'
+import { Button, Card, Col, Input, message, Row, Typography } from 'antd'
 import axios from 'axios'
+import { useState } from 'react'
 import AppContainer from '../../layouts/App'
 
 const API_HOST = process.env.API_HOST || 'http://localhost:8000'
-const { Title } = Typography
+const { Title, Text } = Typography
 
 export async function getServerSideProps({ params }) {
   const { uid } = params
@@ -16,22 +17,96 @@ export async function getServerSideProps({ params }) {
       author: username,
       description,
       name,
+      uid,
     },
   }
 }
 
 export default function ViewModel(props) {
-  const { author, description, name } = props
+  const { author, description, name, uid } = props
+  const [modelInput, setModelInput] = useState('')
+  const [showResults, setShowResults] = useState(false)
+  const [result, setResult] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const executeModel = () => {
+    // Parse array from input
+    let xTest
+    try {
+      xTest = JSON.parse(modelInput)
+      if (typeof xTest !== typeof []) throw 'Specified value is not an array.'
+    } catch {
+      message.error({ content: 'Invalid. Only 2-D arrays are a valid input.' })
+      return
+    }
+
+    setLoading(true)
+    setShowResults(false)
+
+    const url = new URL('/predict', API_HOST).href
+    axios
+      .post(url, { model_uid: uid, xtest: xTest })
+      .then(({ data }) => {
+        setResult(data['prediction'])
+        setLoading(false)
+        setShowResults(true)
+      })
+      .catch(() => {
+        setLoading(false)
+        message.error({
+          content: "Input shape isn't compatible with the model.",
+        })
+      })
+  }
+
   return (
     <AppContainer open={true}>
       <Row justify="center">
         <Col xs={22} sm={20} md={20} lg={18} xl={18} xxl={16}>
           <Title level={2}>{name}</Title>
           <Title level={5}>
-            by user <b>@{author}</b>
+            by <b>@{author}</b>
           </Title>
-          <Card title="Model Description" style={{ marginTop: 40 }}>
+          <Text>
+            Model ID -
+            <Text code copyable>
+              {uid}
+            </Text>
+          </Text>
+          <Card
+            title={<Text type="secondary">Description</Text>}
+            style={{ marginTop: 40 }}
+          >
             <p>{description}</p>
+          </Card>
+
+          <Card
+            title={<Text type="secondary">Try it</Text>}
+            style={{ marginTop: 40 }}
+          >
+            <Row justify="center" gutter={[10, 10]}>
+              <Col flex="auto">
+                <Input
+                  placeholder="Model input, an array within [...]"
+                  onChange={(e) => setModelInput(e.target.value)}
+                />
+              </Col>
+              <Col flex="auto" span={6}>
+                <Button onClick={executeModel} type="primary" loading={loading}>
+                  Execute
+                </Button>
+              </Col>
+            </Row>
+            <Row style={{ marginTop: 24, display: showResults ? '' : 'none' }}>
+              <Col span={24}>
+                <div style={{ backgroundColor: '#eee9', padding: 10 }}>
+                  <Title level={3} keyboard>
+                    Result
+                  </Title>
+                  <Text code>{result}</Text>
+                </div>
+              </Col>
+            </Row>
           </Card>
         </Col>
       </Row>
